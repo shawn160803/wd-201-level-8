f30ad963b321981e3e237cc9e7df9222c1041416
shawn160803 <113343054+shawn160803@users.noreply.github.com>
2022-12-21 17:24:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,12 @@
 const {request, response} = require('express');
 const express = require('express');
 const app = express();
-const csrf = require('csurf');
 
 const {Todo} = require('./models');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-app.use(express.urlencoded({extended: false}));
 const path = require('path');
 
 app.use(bodyParser.json());
-app.use(cookieParser('ssh!!!! some secret string'));
-app.use(csrf({cookie: true}));
 
 
 // seting the ejs is the engine
@@ -21,16 +15,12 @@ app.set('view engine', 'ejs');
 
 app.get('/', async (request, response)=>{
   const allTodos = await Todo.getTodos();
-  const overdue = await Todo.overdue();
-  const dueToday = await Todo.dueToday();
-  const dueLater = await Todo.dueLater();
   if (request.accepts('html')) {
     response.render('index', {
-      allTodos, overdue, dueToday, dueLater,
-      csrfToken: request.csrfToken(),
+      allTodos,
     });
   } else {
-    response.json({allTodos, overdue, dueToday, dueLater});
+    response.json({allTodos});
   }
 });
 
@@ -46,7 +36,7 @@ app.post('/todos', async (request, response)=>{
     const todo =await Todo.addTodo({
       title: request.body.title, dueDate: request.body.dueDate,
     });
-    return response.redirect('/');
+    return response.json(todo);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
