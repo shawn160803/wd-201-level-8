03022e4ed67922df1408f5a51f6208d003450cf3
shawn160803 <113343054+shawn160803@users.noreply.github.com>
2022-12-22 21:03:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const {request, response} = require('express');
 const express = require('express');
 const app = express();
-const csrf = require('csurf');
+const csrf = require('tiny-csrf');
 
 const {Todo} = require('./models');
 const bodyParser = require('body-parser');
@@ -13,7 +13,7 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(cookieParser('ssh!!!! some secret string'));
-app.use(csrf({cookie: true}));
+app.use(csrf('this_should_be_32_character_long',['POST','PUT','DELETE']));
 
 
 // seting the ejs is the engine
