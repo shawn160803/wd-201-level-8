10ad6c1c22eedfc54411910c14cf5895adf1021a
shawn160803 <113343054+shawn160803@users.noreply.github.com>
2022-12-22 21:59:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,4 @@
-/* eslint-disable no-unused-vars */
-const {request, response} = require('express');
-const express = require('express');
-const app = express();
-const csrf = require('tiny-csrf');
-
-const {Todo} = require('./models');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-app.use(express.urlencoded({extended: false}));
-const path = require('path');
-
-app.use(bodyParser.json());
-app.use(cookieParser('ssh!!!! some secret string'));
-app.use(csrf('this_should_be_32_character_long',['POST','PUT','DELETE']));
-
-
-// seting the ejs is the engine
-app.set('view engine', 'ejs');
-
-app.get('/', async (request, response)=>{
-  const allTodos = await Todo.getTodos();
-  const overdue = await Todo.overdue();
-  const dueToday = await Todo.dueToday();
-  const dueLater = await Todo.dueLater();
-  if (request.accepts('html')) {
-    response.render('index', {
-      allTodos, overdue, dueToday, dueLater,
-      csrfToken: request.csrfToken(),
-    });
-  } else {
-    response.json({allTodos, overdue, dueToday, dueLater});
-  }
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/todos', (request, response)=>{
-  console.log('Todo List', request.body);
-});
-app.post('/todos', async (request, response)=>{
-  console.log('Todo List');
-  try {
-    console.log('entering in try block');
+entering in try block');
     const todo =await Todo.addTodo({
       title: request.body.title, dueDate: request.body.dueDate,
     });
